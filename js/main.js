c700c4177f7d5b7ff4e056dce19b86fe689fde71
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-02-26 19:10:32 +0300

    add

--- js/main.js
+++ js/main.js
@@ -21,32 +21,7 @@ let animFirst = anime({
   let secondClick = document.getElementById('secondClick')
   secondClick.onclick = animSecond.play
 
-/*
-let prevRotate = 0
-  let secondClick = document.getElementById('secondClick')
-  secondClick.onclick = function() {
-    anime({
-      targets: '.triangle',
-      scale: 0.5,
-      rotate: function () {
-        return  prevRotate + anime.random(-25,100)
-      },
-      changeComplete: function(el) {
-        prevRotate = Number(el.animations[0].currentValue.slice(0,4))
-      }
-    })
-  }
-  */
-/*
-  let animSecond = anime.set('.triangle', {
-    rotate: function() { return anime.random(0, 360); },
-  });
-  let secondClick = document.getElementById('secondClick')
-  secondClick.onclick = animSecond
-  */
-
-
-  //for small blue triangle 
+  //for small blue triangle
 let animThird = anime({
   targets: '.triangle__small',
   rotate: ['86.74deg',  360],
@@ -93,44 +68,7 @@ animFifth
 })
 
 //for rigth black square and small blue rec
-
-/*
-let animSix = anime({
-  targets: '.square__black',
-  keyframes: [
-    {translateY: -40},
-    {translateX: 250},
-    {translateY: 40},
-    {translateX: 0},
-    {translateY: 0}
-  ],
-  duration: 4000,
-  direction: 'alternate',
-  easing: 'easeInOut',
-  loop: true,
-  autoplay: true
-});
-*/
-
-/*
-let animSix = anime({
-  targets: '.square__black',
-  translateX: -730,
-  translateY: 15,
-  backgroundColor: 'none',
-  border: 'solid #000',
-  borderRadius: [0, '50%'],
-  rotate: [45, 270],
-  scale: 2,
-  backgroundColor: 'rgba(0, 0, 0, 0)',
-  boxShadow: '0px 0px 0px 2px #0034EB',
-  easing: 'steps(5)',
-  autoplay: false
-})
-let sixClick = document.getElementById('sixClick')
-sixClick.onclick = animSix.play
-
-let animSeven = anime({
+let animSix  = anime({
   targets: '.rec__blue__small',
   keyframes: [
     {translateY: -40},
@@ -145,59 +83,10 @@ let animSeven = anime({
   ],
   duration: 4000,
   easing: 'easeOutElastic(1, .8)',
-  loop: true,
-  autoplay: false
-})
-*/
-
-let animSix  = anime.timeline({
-  easing: 'linear',
-  delay: 5000,
-  duration: 1500
+  delay: 7000,
+  duration: 5000
 });
 
-animSix
-.add({
-  targets: '.square__black',
-  translateX: -730,
-  translateY: 15,
-  backgroundColor: 'none',
-  border: 'solid #000',
-  borderRadius: [0, '50%'],
-  rotate: [45, 270],
-  scale: 2,
-  backgroundColor: 'rgba(0, 0, 0, 0)',
-  boxShadow: '0px 0px 0px 2px #0034EB',
-  easing: 'steps(5)'
-})
-.add({
-  targets: '.rec__blue__small',
-  keyframes: [
-    {translateY: -40},
-    {translateX: -600},
-    {backgroundColor: '#000'},
-    {scale: 2},
-    {translateY: 40},
-    {scale: 1},
-    {backgroundColor: '#0034EB'},
-    {translateX: 0},
-    {translateY: 0},
-  ],
-  duration: 4000,
-  easing: 'easeOutElastic(1, .8)'
-})
-/*
-let sixClick = document.getElementsByClassName('group')
-sixClick.onclick = animSix.play
-*/
-/*
-let sevenClick = document.getElementById('sevenClick')
-function playGroup() {
-  if (animSix.play == true) {
-   return  sevenClick.onclick = animSeven.play
-  }
-}
-*/
 
 let eightAnim = anime({
   targets: '.circle__down',
@@ -209,3 +98,12 @@ let eightAnim = anime({
 })
 let eightClick = document.getElementById('eigthtClick')
 eigthtClick.onclick = eightAnim.play
+
+
+//Подключаю библиотеку прокрутки
+let controller = new ScrollMagic.Controller()
+
+let square = document.querySelector('.yellow__square__one')
+let squareOneAnim = anime({
+  targets: square
+})
